05fdc4e5cee0de9a1cf183e7e6ab86fa4319b4aa
Giftie23 <giftieobetn@gmail.com>
2023-02-17 17:21:32 -0800

    task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit nos of base 10
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	int f;
+
+	for (f = 0; f < 10; f++)
+		printf("%d", f);
+	putchar('\n');
+	return (0);
+}
