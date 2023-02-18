b61d5f46b58c83d23fbbd3f327d74db98775df27
Giftie23 <giftieobetn@gmail.com>
2023-02-17 23:56:09 -0800

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Alx its not really easy but i go finish
+ *
+ * Return: 0 ()
+ *
+ */
+int main(void)
+{
+	int d;
+
+	while
+		(d < 10)
+		{
+			putchar(d + '0');
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
