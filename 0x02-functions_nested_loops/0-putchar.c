1fa6ddffb19a842666db512ebf0e2880e651fca4
Giftie23 <giftieobetn@gmail.com>
2023-02-22 04:58:00 -0800

    task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,21 @@
 #include "main.h"
-
 /**
-* main - Prints putchar
-* Return: Always 0
-*
-*/
+ * main - mehn this task wasn't easy o
+ *
+ * Return: 0 (Success)
+ *
+ */
+
 int main(void)
 {
-
-	_putchar('_');
-       	_putchar('p');
-	_putchar('u');
-       	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
-
-
